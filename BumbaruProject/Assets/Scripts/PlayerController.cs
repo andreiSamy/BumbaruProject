8f76db23a5d9bde03c4a6d34935f7fcb5faf79d4
Claudiu <claudiucobzaru11@gmail.com>
2023-04-04 00:00:04 +0300

    Animations
    wow ce tare sunt

--- BumbaruProject/Assets/Scripts/PlayerController.cs
+++ BumbaruProject/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
+    public Animator animator;
     public CharacterController controller;
     private Vector3 direction;
     public float speed = 8f;
@@ -12,6 +13,7 @@ public class PlayerController : MonoBehaviour
     public float gravity = -20f;
     public Transform groundCheck;
     public LayerMask groundLayer;
+    public Transform model;
 
     void Start()
     {
@@ -21,9 +23,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        
         float hInput = Input.GetAxis("Horizontal");
         direction.x = hInput * speed;
 
+
+        animator.SetFloat("speed", Mathf.Abs(hInput));
         bool isGrounded = Physics.CheckSphere(groundCheck.position, 0.15f, groundLayer);
 
         direction.y += gravity * Time.deltaTime;
@@ -35,6 +41,11 @@ public class PlayerController : MonoBehaviour
             direction.y = jumpForce;
         
             }
+        }
+        if(hInput !=0)
+        {
+            Quaternion newRotation = Quaternion.LookRotation(new Vector3(hInput, 0, 0));
+            model.rotation = newRotation;
         }
 
 
